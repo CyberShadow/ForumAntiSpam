ac39d427dedc0ecd724301f17d6353f0a82dbf1f
Vladimir Panteleev <vladimir@thecybershadow.net>
2011-06-07 15:04:58 +0300

    AntiSpam: add logging, actually delete posts

--- AntiSpam.d
+++ AntiSpam.d
@@ -1,11 +1,20 @@
 import std.c.time;
 import std.stdio;
+import std.string;
+
+import Team15.Logging;
+import Team15.CommandLine;
 
 import Forum;
 import SpamEngines;
 
-void main()
+const TOTAL_POSITIVE_THRESHOLD = 2; // at least this many spam checkers must return a positive to delete this post
+
+void main(string[] args)
 {
+	parseCommandLine(args);
+	auto log = createLogger("AntiSpam");
+
 	login();
 
 	bool[string] knownIDs;
@@ -15,12 +24,30 @@ void main()
 		foreach (ID; IDs)
 			if (!(ID in knownIDs))
 			{
-				writefln("Checking post %s", ID);
+				log("Checking post " ~ ID);
 				auto post = getPost(ID);
+				log("Author: " ~ post.author);
+				log("IP: " ~ post.IP);
+				log("Title: " ~ post.title);
+				log("Content:\n" ~ post.message);
+
+				string[] positiveEngines;
 				foreach (name, checker; engines)
 					with (checker(post.tupleof))
-						writefln("%-20s: %s%s", name, isSpam ? "SPAM" : "not spam", details ? " (" ~ details ~ ")" : "");
+					{
+						log(format("%-20s: %s%s", name, isSpam ? "SPAM" : "not spam", details ? " (" ~ details ~ ")" : ""));
+						positiveEngines ~= name;
+					}
 
+				if (positiveEngines.length >= TOTAL_POSITIVE_THRESHOLD)
+				{
+					log("Deleting...");
+					deletePost(ID, "Spam (" ~ positiveEngines.join(", ") ~ ")");
+				}
+				else
+					log("Not deleting.");
+
+				log("---------------------------------");
 				knownIDs[ID] = true;
 			}
 
