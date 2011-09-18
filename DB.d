9d312bdf54f49af33f091bd8458b49f55fafedeb
Vladimir Panteleev <vladimir@thecybershadow.net>
2011-09-19 01:26:15 +0300

    Remove redundant indexes

--- DB.d
+++ DB.d
@@ -10,10 +10,8 @@ static this()
 {
 	db = new SQLite("spam.sqlite3");
 
-	db.exec("CREATE TABLE IF NOT EXISTS `posts` (`id` INTEGER, `time`, `author`, `IP`, `title`, `text`, `verdict`, UNIQUE(`id`, `time`))");
-	db.exec("CREATE INDEX IF NOT EXISTS `post` ON `posts` (`id`)");
-	db.exec("CREATE TABLE IF NOT EXISTS `results` (`id` INTEGER, `engine`, `time`, `result`, `details`, `session`, UNIQUE(`id`, `engine`, `time`))");
-	db.exec("CREATE INDEX IF NOT EXISTS `result` ON `results` (`id`, `engine`)");
+	db.exec("CREATE TABLE IF NOT EXISTS `posts` (`id` INTEGER, `time`, `author`, `IP`, `title`, `text`, `verdict`, PRIMARY KEY(`id`, `time`))");
+	db.exec("CREATE TABLE IF NOT EXISTS `results` (`id` INTEGER, `engine`, `time`, `result`, `details`, `session`, PRIMARY KEY(`id`, `engine`, `time`))");
 
 	newPost    = db.prepare("INSERT INTO `posts`(`id`, `time`, `author`, `IP`, `title`, `text`, `verdict`) VALUES (?, ?, ?, ?, ?, ?, ?)");
 	newResult  = db.prepare("INSERT INTO `results`(`id`, `engine`, `time`, `result`, `details`, `session`) VALUES (?, ?, ?, ?, ?, ?)");
